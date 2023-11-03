8cf7bce93b6df389f9c45824e48617ab797be9c1
Adam Pigg <adam@piggz.co.uk>
2023-11-03 22:38:09 +0000

    Add support for the pinetab2

--- flash-it.sh
+++ flash-it.sh
@@ -9,17 +9,21 @@ ROOTFS_PINEPHONE_1_1_JOB=pinephone-1.1-rootfs
 ROOTFS_PINETAB_JOB=pinetab-rootfs
 ROOTFS_PINETABDEV_JOB=pinetab-rootfs
 ROOTFS_PINEPHONEPRO_JOB=pinephonepro-rootfs
+ROOTFS_PINETAB2_JOB=pinetab2-rootfs
+
 ROOTFS_PINEPHONE_1_0_DIR=pinephone-1.0
 ROOTFS_PINEPHONE_1_1_DIR=pinephone-1.1
 ROOTFS_PINETAB_DIR=pinetab
 ROOTFS_PINETABDEV_DIR=pinetab
 ROOTFS_PINEPHONEPRO_DIR=pinephonepro
+ROOTFS_PINETAB2_DIR=pinetab2
 
 UBOOT_PINEPHONE_1_0_FILE=boot.pinephone10.scr
 UBOOT_PINEPHONE_1_1_FILE=boot.pinephone11.scr
 UBOOT_PINETAB_FILE=boot.pinetab.scr
 UBOOT_PINETABDEV_FILE=boot.pinetabdev.scr
 UBOOT_PINEPHONEPRO_FILE=boot.pinephonepro.scr
+UBOOT_PINETAB2_FILE=boot.pinetab2.scr
 
 MOUNT_ROOT=./root
 MOUNT_BOOT=./boot
@@ -155,13 +159,14 @@ echo ""
 
 # Image selection
 echo -e "\e[1mWhich image do you want to flash?\e[0m"
-select OPTION in "PinePhone 1.0 (Development) device" "PinePhone 1.1 (Brave Heart) or 1.2 (Community Editions) device" "PineTab device" "PineTab Dev device" "Pinephone Pro"; do
+select OPTION in "PinePhone 1.0 (Development) device" "PinePhone 1.1 (Brave Heart) or 1.2 (Community Editions) device" "PineTab device" "PineTab Dev device" "Pinephone Pro" "Pinetab 2"; do
     case $OPTION in
         "PinePhone 1.0 (Development) device" ) ROOTFS_JOB=$ROOTFS_PINEPHONE_1_0_JOB; ROOTFS_DIR=$ROOTFS_PINEPHONE_1_0_DIR; UBOOT_FILE=$UBOOT_PINEPHONE_1_0_FILE; break;;
         "PinePhone 1.1 (Brave Heart) or 1.2 (Community Editions) device" ) ROOTFS_JOB=$ROOTFS_PINEPHONE_1_1_JOB; ROOTFS_DIR=$ROOTFS_PINEPHONE_1_1_DIR; UBOOT_FILE=$UBOOT_PINEPHONE_1_1_FILE; break;;
         "PineTab device" ) ROOTFS_JOB=$ROOTFS_PINETAB_JOB; ROOTFS_DIR=$ROOTFS_PINETAB_DIR; UBOOT_FILE=$UBOOT_PINETAB_FILE; break;;
         "PineTab Dev device" ) ROOTFS_JOB=$ROOTFS_PINETABDEV_JOB; ROOTFS_DIR=$ROOTFS_PINETABDEV_DIR; UBOOT_FILE=$UBOOT_PINETABDEV_FILE; break;;
         "Pinephone Pro" ) ROOTFS_JOB=$ROOTFS_PINEPHONEPRO_JOB; ROOTFS_DIR=$ROOTFS_PINEPHONEPRO_DIR; UBOOT_FILE=$UBOOT_PINEPHONEPRO_FILE; break;;
+        "Pinetab 2" ) ROOTFS_JOB=$ROOTFS_PINETAB2_JOB; ROOTFS_DIR=$ROOTFS_PINETAB2_DIR; UBOOT_FILE=$UBOOT_PINETAB2_FILE; break;;
     esac
 done
 
@@ -254,21 +259,36 @@ sudo mount $BOOTPART "$MOUNT_BOOT" # Mount boot partition
 echo "Boot partition mount: $MOUNT_BOOT"
 sudo sh -c "cp -r $MOUNT_ROOT/boot/* $MOUNT_BOOT"
 
-echo `ls $MOUNT_BOOT`
-echo -e "\e[1mCopying UBoot script to boot partition...\e[0m"
-if [ "$CUSTOM" != "" ]; then
-    sudo sh -c "cp '${CUSTOM}/boot.scr' '$MOUNT_BOOT/boot.scr'"
+
+#Only copy boot script if not Pinetab2
+if [ "$OPTION" != "Pinetab 2" ]; then
+    echo `ls $MOUNT_BOOT`
+    echo -e "\e[1mCopying UBoot script to boot partition...\e[0m"
+    if [ "$CUSTOM" != "" ]; then
+        sudo sh -c "cp '${CUSTOM}/boot.scr' '$MOUNT_BOOT/boot.scr'"
+    else
+        sudo sh -c "cp '$MOUNT_BOOT/$UBOOT_FILE' '$MOUNT_BOOT/boot.scr'"
+    fi
+    sync
 else
-    sudo sh -c "cp '$MOUNT_BOOT/$UBOOT_FILE' '$MOUNT_BOOT/boot.scr'"
+    #Flash the uboot supplied with the release
+    if [ -f "$MOUNT_BOOT/u-boot-rockchip.bin" ]; then
+        read -p "Do you want to flash uboot? " yn
+        case $yn in
+            [Yy]* ) sudo dd if="$MOUNT_BOOT/u-boot-rockchip.bin" of="${DEVICE_NODE}" oflag=direct seek=64
+        esac
+    fi
+    #Rewrite the boot config
+    PARTUUID=`sudo blkid $ROOTPART -s PARTUUID -o value`
+    sed "s/ROOTUUID/$PARTUUID/" $MOUNT_BOOT/extlinux/extlinux.conf.in | sudo tee $MOUNT_BOOT/extlinux/extlinux.conf > /dev/null;
 fi
-sync
 
 #Rewrite the home config
 read -p "Are you installing to an SD card? " yn
-if [ "$OPTION" != "Pinephone Pro" ]; then
-    DEVICESD="mmcblk0"
-else
+if [ "$OPTION" == "Pinephone Pro" ]; then
     DEVICESD="mmcblk1"
+else
+    DEVICESD="mmcblk0"
 fi
 case $yn in
 	[Yy]* ) sudo sed -i "s/mmcblk2/$DEVICESD/" $MOUNT_ROOT/etc/sailfish-device-encryption-community/devices.ini;
